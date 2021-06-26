e8b8388cd842cc035407e80bd9e8dd786c7ca470
Douglas <phena109@gmail.com>
2021-06-26 21:29:59 +1000

    UI Setup

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -25,6 +25,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->render('index', [
+            'transactions' => []
+        ]);
     }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,33 @@
-Hello World
+<h1>Upload new CSV</h1>
+
+<p>Select CSV to upload</p>
+
+<form action="" method="post" enctype='multipart/form-data'>
+    <input type="file" name="csv_file">
+    <button type="submit">Upload CSV</button>
+</form>
+
+<h1>Bank Transactions from CSV</h1>
+
+<table>
+    <thead>
+    <tr>Date</tr>
+    <tr>Transaction Code</tr>
+    <tr>Valid Transaction?</tr>
+    <tr>Customer Number</tr>
+    <tr>Reference</tr>
+    <tr>Amount</tr>
+    </thead>
+    <?php
+    foreach ($transactions as $transaction) {
+        echo "<tr>";
+        echo "<td></td>";
+        echo "<td></td>";
+        echo "<td></td>";
+        echo "<td></td>";
+        echo "<td></td>";
+        echo "<td></td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
